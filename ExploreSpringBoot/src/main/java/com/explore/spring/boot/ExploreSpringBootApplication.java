2ac5a590c1779aee675e10bcb7d51a777924e9b1
DineshKalimuthu <dinesh_kalimuthu@capturisk.com>
2022-06-22 06:33:50 +0530

    Initial Commit

---
+++ ExploreSpringBoot/src/main/java/com/explore/spring/boot/ExploreSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.explore.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreSpringBootApplication.class, args);
+	}
+
+}
---
+++ ExploreSpringBoot/src/test/java/com/explore/spring/boot/ExploreSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.explore.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
